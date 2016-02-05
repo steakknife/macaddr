99017c029321a358cd5734a8a921097ac9476c6e
John V. Pataki <john.pataki@logicallabs.com>
2016-02-05 15:26:41 -0800

    Add a fallback for windows to get mac address using either getmac or ipconfig /all. Also should handle fallback for when not on windows and still need another method using ifconfig.

--- lib/macaddr.rb
+++ lib/macaddr.rb
@@ -22,7 +22,7 @@ module Mac
   # @return [Array<String>] All hardware address,
   #         in no particular order (varies by system)
   def addresses
-    iface_macs.values.compact || []
+    addresses = iface_macs.values.compact || []
   end
 
   alias_method :mac_address, :address
@@ -31,7 +31,7 @@ module Mac
  
   # @return [Array<Ifaddr>] Return all interface Ifaddrs
   def ifaddrs
-    return unless Socket.respond_to? :getifaddrs
+    return [] if !Socket.respond_to?(:getifaddrs) || INTERFACE_PACKET_FAMILY.nil?
     Socket.getifaddrs.select do |iface|
       iface.addr && iface.addr.pfamily == INTERFACE_PACKET_FAMILY
     end
@@ -46,17 +46,22 @@ module Mac
     Hash[h]
   end
 
-  private
+  # private
  
-  INTERFACE_PACKET_FAMILY = Socket::PF_LINK rescue (Socket::PF_PACKET)
+  INTERFACE_PACKET_FAMILY = Socket.const_defined?(:PF_LINK) ? Socket::PF_LINK : 
+    Socket.const_defined?(:PF_PACKET) ? Socket::PF_PACKET : nil
   EMPTY_MAC = '00:00:00:00:00:00'
   HWADDR_REGEX = /hwaddr=([\h:]+)/
+  PLATFORM = RUBY_PLATFORM.downcase
+  WINDOWS = (PLATFORM =~ /mingw|mswin/)
 
   def iface_macs_raw
     if Socket.const_defined? :PF_LINK
       from_getnameinfo
-    else
+    elsif Socket.const_defined? :PF_PACKET
       from_inspect_sockaddr
+    else 
+      (WINDOWS ? for_windows : from_ifconfig)
     end
   end
  
@@ -73,6 +78,39 @@ module Mac
       [iface.name, mac]
     end
   end
+  
+  def for_windows
+    (%x{where getmac}).empty? ? from_ipconfig : from_getmac
+  end
+  
+  def clean arg
+    arg.gsub(/\"/,'')
+  end
+  
+  def from_getmac 
+    getmac = %x{getmac /fo CSV /nh}
+    interfaces = getmac.split(/\n/)
+    interfaces.collect do |interface| 
+      mac,name = interface.split(/,/)
+      [clean(name),clean(mac)]
+    end
+  end
+  
+  # untested and stolen from http://sketchucation.com/forums/viewtopic.php?f=180&t=55422#p504017
+  def from_ipconfig_or_ifconfig    
+    iptxt = %x{#{WINDOWS ? 'ipconfig /all' : 'ifconfig'}}
+    ## delete DHCPv6 :
+    iptxt.gsub!(/..\-..\-..\-..\-..\-..\-..\-..\-..\-..\-..\-..\-..\-../,"") 
+    # delete Tunnel :
+    iptxt.gsub!("00-00-00-00-","")
+    ## create array with all the physical adresses :
+    iptxt.scan(/..\-..\-..\-..\-..\-../).collect do |mac| 
+      ["ignore", mac]
+    end
+  end  
+  alias_method :from_ipconfig, :from_ipconfig_or_ifconfig
+  alias_method :from_ifconfig, :from_ipconfig_or_ifconfig
+  
 end
 
 MacAddr = Macaddr = Mac
